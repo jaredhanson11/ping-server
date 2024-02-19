13da965ba8033a7cd201e09be82a4aa94667b658
Jared Hanson <jred0011@gmail.com>
2024-02-19 14:34:05 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16-slim
+WORKDIR /app/
+COPY ./ /app/
+RUN yarn
+
+ENTRYPOINT ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,42 @@
+// AVAILABLE ENV VARIABLES
+// PORT: integer for port to run the server
+// LOG_INTERVAL: integer for number of seconds between logs of number of requests
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT ?? 3500;
+
+let reqs = 0;
+
+let dateFormatter = Intl.DateTimeFormat("en-us", {
+  timeZone: "PST",
+  dateStyle: "medium",
+  timeStyle: "long",
+});
+setInterval(
+  () => {
+    console.log(
+      `Time: ${dateFormatter.format(new Date())}\nNumber of requests: ${reqs}`
+    );
+  },
+  process.env.LOG_INTERVAL ? parseInt(process.env.LOG_INTERVAL) * 1000 : 5000
+);
+
+app.get("/", (req, res) => {
+  reqs++;
+  res.end();
+});
+
+app.get("/healthz", (req, res) => {
+  reqs++;
+  res.end();
+});
+
+app.get("/reset", (req, res) => {
+  reqs = 0;
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
